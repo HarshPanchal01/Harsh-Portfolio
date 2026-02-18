e196a7062cdaf4c12d6cb1a8f11449492334ffa9
Harsh Panchal <harshpanchal30x@gmail.com>
2026-02-17 19:36:09 -0500

    new info and assets

---
+++ .svelte-kit/non-ambient.d.ts
@@ -0,0 +1,41 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/";
+		RouteParams(): {
+			
+		};
+		LayoutParams(): {
+			"/": Record<string, never>
+		};
+		Pathname(): "/";
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/assets/Harsh_Panchal_Resume_2025.pdf" | "/assets/eclipsecreations.jpg" | "/assets/elephant.svg" | "/assets/email.svg" | "/assets/github.svg" | "/assets/hellochef.png" | "/assets/jobfinder.png" | "/assets/linkedin.svg" | "/assets/manulife.jpeg" | "/assets/ontariohealth.jpeg" | "/assets/sunlife.png" | "/robots.txt" | string & {};
+	}
+}
